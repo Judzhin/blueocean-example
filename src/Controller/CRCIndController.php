f7bf3d2c1309df57374f49ebe5937fba4e00d4f5
Judzhin Miles <info@msbios.com>
2021-07-01 09:32:03 +0300

    Count

--- src/Controller/CRCIndController.php
+++ src/Controller/CRCIndController.php
@@ -2,19 +2,36 @@
 
 namespace App\Controller;
 
+use App\Service\CRCIndClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class CRCIndController
+ *
+ * @package App\Controller
+ */
 class CRCIndController extends AbstractController
 {
+    /**
+     * @param CRCIndClient $client
+     * @return Response
+     */
     #[Route('/crcind', name: 'crcind')]
-    public function index(): Response
+    public function index(CRCIndClient $client): Response
     {
-        $soapClient = new \SoapClient('https://www.crcind.com/csp/samples/SOAP.Demo.CLS?WSDL');
-        $result = $soapClient->GetByName('a');
-
-        dd($result);
+        // $soapClient = new \SoapClient('https://www.crcind.com/csp/samples/SOAP.Demo.CLS?WSDL');
+        $query = strtolower('a');
+        $total = 0;
+        $result = $client->GetListByName($query);
+        dd($result->GetListByNameResult->PersonIdentification);
+        foreach ($result->GetListByNameResult->PersonIdentification as $personIdentification)
+        {
+            if (strtolower($personIdentification->Name[0]) === $query) {
+                ++$total;
+            }
+        }
 
         return $this->render('crcind/index.html.twig', [
             'controller_name' => 'CRCIndController',
---
+++ src/Service/CRCIndClient.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Service;
+
+/**
+ * Class CRCIndClient
+ * @package App\Service
+ */
+class CRCIndClient extends \SoapClient
+{
+    ///**
+    // * CRCIndClient constructor.
+    // * @param $wsdl
+    // * @param array|null $options
+    // * @throws \SoapFault
+    // */
+    //public function __construct($wsdl, array $options = null)
+    //{
+    //    parent::__construct($wsdl, $options);
+    //    dump($this->__getFunctions());
+    //    dd($this->__getTypes());
+    //}
+}
