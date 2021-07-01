64b69383a286e8f9e1320ff805387192d740df15
Judzhin Miles <info@msbios.com>
2021-07-01 12:49:38 +0300

    Test 01

--- src/Controller/CRCIndController.php
+++ src/Controller/CRCIndController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractObjectNormalizer;
 use Symfony\Component\Serializer\Normalizer\PropertyNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
@@ -28,23 +29,37 @@ class CRCIndController extends AbstractController
         $result = $client->GetListByName(['name' => 'a']);
         /** @var PersonIdentification[]|array $persons */
 
-        /**
-         * @param array $result
-         * @return iterable
-         * @throws ExceptionInterface
-         */
-        $deNormalize = function (array $result): iterable {
-            $normalizer = new PropertyNormalizer;
-            $serializer = new Serializer([$normalizer]);
-
-            foreach ($result as $PersonIdentification) {
-                yield $serializer->denormalize($PersonIdentification, PersonIdentification::class);
-            }
-        };
+        // /**
+        //  * @param array $result
+        //  * @return iterable
+        //  * @throws ExceptionInterface
+        //  */
+        // $deNormalize = function (array $result): iterable {
+        //     $normalizer = new PropertyNormalizer;
+        //     $serializer = new Serializer([$normalizer]);
+        //
+        //     foreach ($result as $PersonIdentification) {
+        //         yield $serializer->denormalize($PersonIdentification, PersonIdentification::class);
+        //     }
+        // };
 
         return $this->render('crcind/index.html.twig', [
-            'persons' => iterator_to_array($deNormalize($result->GetListByNameResult->PersonIdentification)),
+            'persons' => iterator_to_array($this->deNormalize($result->GetListByNameResult->PersonIdentification)),
         ]);
     }
 
+    /**
+     * @param array $result
+     * @return iterable
+     * @throws ExceptionInterface
+     */
+    private function deNormalize(array $result): iterable
+    {
+        $normalizer = new PropertyNormalizer;
+        $serializer = new Serializer([$normalizer]);
+
+        foreach ($result as $PersonIdentification) {
+            yield $serializer->denormalize($PersonIdentification, PersonIdentification::class);
+        }
+    }
 }
